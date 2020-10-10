1998456938f044f556c26e565f99de0285929d5c
karla2110 <karla02651@gmail.com>
2020-10-10 00:28:48 -0500

    Ptoyecto1

--- ASPCore/Controllers/HomeController.cs
+++ ASPCore/Controllers/HomeController.cs
@@ -9,9 +9,53 @@ using ASPCore.Models;
 namespace ASPCore.Controllers
 {
     public class HomeController : Controller
+
     {
         public IActionResult Index()
         {
+            Array array;
+            List<AlumnoModel> alumnos=new List<AlumnoModel>();
+
+            alumnos.Add(new AlumnoModel
+            {
+                NC = "17030084",
+                Nombre = "Karla",
+                App = "Garcia",
+                Apm = "Reza",
+                Carrera = "Informatica",
+                Semestre = 7,
+                Promedio = 9.54,
+                FechaIngreso = DateTime.Now
+            });
+
+            alumnos.Add(new AlumnoModel
+            {
+                NC = "17030082",
+                Nombre = "Jaqueline",
+                App = "Fernnandez",
+                Apm = "Maldonado",
+                Carrera = "Informatica",
+                Semestre = 7,
+                Promedio = 9.55,
+                FechaIngreso = DateTime.Now
+            });
+
+            alumnos.Add(new AlumnoModel
+            {
+                NC = "17030081",
+                Nombre = "Carlos",
+                App = "Cordova",
+                Apm = "Lechuga",
+                Carrera = "Informatica",
+                Semestre = 7,
+                Promedio = 9.56,
+                FechaIngreso = DateTime.Now
+            });
+
+            array = alumnos.ToArray();
+            ViewBag.datos = array;
+                       
+
             return View();
         }
 
---
+++ ASPCore/Models/AlumnoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore.Models
+{
+    public class AlumnoModel
+    {
+        
+        public string Carrera { get; set; }
+        public double Promedio { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string NC { get; set; }
+        public string Nombre { get; set; }
+        public string App { get; set; }
+        public string Apm { get; set; }
+        public int Semestre { get; set; }
+
+
+    }
+}
